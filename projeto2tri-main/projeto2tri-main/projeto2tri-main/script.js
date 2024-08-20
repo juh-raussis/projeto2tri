f3c1be7d02243dd744f44f163efdad142cfaecda
juh-raussis <145705147+juh-raussis@users.noreply.github.com>
2024-08-20 08:45:40 -0300

    Add files via upload

--- projeto2tri-main/projeto2tri-main/projeto2tri-main/script.js
+++ projeto2tri-main/projeto2tri-main/projeto2tri-main/script.js
@@ -74,7 +74,6 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
@@ -82,20 +81,15 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        botaoAlternativas.addEventListener("click", function () {
+            atual++;
+            mostraPergunta();
+        })
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacoes;
-    historiaFinal = afirmacoes;
-    atual++;
-    mostraPergunta();
-}
-
 mostraPergunta();
